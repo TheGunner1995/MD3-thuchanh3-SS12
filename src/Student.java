b35adefde07420ac8ec45214160a102b51e9651f
Tô Việt Hoàng <totranhieu@gmail.com>
2023-03-24 09:15:05 +0700

    aaaa

---
+++ src/Student.java
@@ -0,0 +1,50 @@
+public class Student implements Comparable<Student>{
+    private String name;
+    private Integer age;
+    private String adress;
+
+    public Student(String name, Integer age, String adress) {
+        this.name = name;
+        this.age = age;
+        this.adress = adress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", adress='" + adress + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        return this.getName().compareTo(student.getName());
+
+    }
+}
